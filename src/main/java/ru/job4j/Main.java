b2d0c59bb17893e963fda648c3bbccc7a997962d
dzhenetl <david.zhenetl@gmail.com>
2022-11-18 02:23:00 +0300

    1. Quartz [#175122]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -1,4 +1,4 @@
-package com.dzhenetl;
+package ru.job4j;
 
 public class Main {
     public static void main(String[] args) {
---
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -0,0 +1,38 @@
+package ru.job4j.quartz;
+
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+import ru.job4j.util.PropertiesUtil;
+
+import static org.quartz.JobBuilder.*;
+import static org.quartz.TriggerBuilder.*;
+import static org.quartz.SimpleScheduleBuilder.*;
+
+public class AlertRabbit {
+    public static void main(String[] args) {
+        int schedule = Integer.parseInt(PropertiesUtil.getProperties("rabbit.properties")
+                .getProperty("rabbit.interval"));
+        try {
+            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+            scheduler.start();
+            JobDetail job = newJob(Rabbit.class).build();
+            SimpleScheduleBuilder times = simpleSchedule()
+                    .withIntervalInSeconds(schedule)
+                    .repeatForever();
+            Trigger trigger = newTrigger()
+                    .startNow()
+                    .withSchedule(times)
+                    .build();
+            scheduler.scheduleJob(job, trigger);
+        } catch (SchedulerException se) {
+            se.printStackTrace();
+        }
+    }
+
+    public static class Rabbit implements Job {
+        @Override
+        public void execute(JobExecutionContext context) {
+            System.out.println("Rabbit runs here ...");
+        }
+    }
+}
