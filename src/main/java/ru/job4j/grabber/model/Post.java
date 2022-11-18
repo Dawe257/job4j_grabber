79ec333d6f16947aacf313d2b4131cb9ca9d30a4
dzhenetl <david.zhenetl@gmail.com>
2022-11-18 17:47:51 +0300

    2.2. Модель данных - Post. [#285211 #362010]

--- src/main/java/ru/job4j/grabber/model/Post.java
+++ src/main/java/ru/job4j/grabber/model/Post.java
@@ -1,78 +1,29 @@
 package ru.job4j.grabber.model;
 
-import java.time.LocalDateTime;
-import java.util.Objects;
+import lombok.*;
 
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
 public class Post {
 
     private int id;
+    @EqualsAndHashCode.Exclude
     private String title;
     private String link;
+    @EqualsAndHashCode.Exclude
     private String description;
+    @EqualsAndHashCode.Exclude
     private LocalDateTime created;
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
+    public Post(String title, String link, String description, LocalDateTime created) {
         this.title = title;
-    }
-
-    public String getLink() {
-        return link;
-    }
-
-    public void setLink(String link) {
         this.link = link;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
         this.description = description;
-    }
-
-    public LocalDateTime getCreated() {
-        return created;
-    }
-
-    public void setCreated(LocalDateTime created) {
         this.created = created;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Post post = (Post) o;
-        return id == post.id && Objects.equals(link, post.link) && Objects.equals(description, post.description);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, link, description);
-    }
-
-    @Override
-    public String toString() {
-        return "Post{" + "id=" + id
-                + ", link='" + link + '\''
-                + ", description='" + description + '\''
-                + ", created=" + created + '}';
     }
 }
