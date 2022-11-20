360694722bef8d697b860e0c2d0a9d66e86ec5af
dzhenetl <david.zhenetl@gmail.com>
2022-11-20 18:19:19 +0300

    4. Схема таблицы Post [#1731 #362119]

--- db/scripts/001_ddl_create_posts_table.sql
+++ db/scripts/001_ddl_create_posts_table.sql
@@ -2,7 +2,7 @@ create table posts
 (
     id          serial primary key,
     title       varchar(255),
-    link        varchar(255),
+    link        varchar(255) unique,
     description varchar,
     created     timestamp
 );
